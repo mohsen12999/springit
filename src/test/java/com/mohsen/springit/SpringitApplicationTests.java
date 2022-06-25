93315c576d14f3774af5e500f7d11c4eaf395172
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-06-26 01:35:31 +0430

    Initial Commit for our Spring Boot Reddit Clone

---
+++ src/test/java/com/mohsen/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohsen.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
