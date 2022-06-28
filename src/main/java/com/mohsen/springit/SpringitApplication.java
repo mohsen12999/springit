17d4f903f016761c8bda08f03b1b84a939bd3075
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-06-28 20:32:15 +0430

    add a config package and read message from config package

--- src/main/java/com/mohsen/springit/SpringitApplication.java
+++ src/main/java/com/mohsen/springit/SpringitApplication.java
@@ -1,14 +1,27 @@
 package com.mohsen.springit;
 
+import com.mohsen.springit.config.SpringitProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
+
+    @Autowired
+    private SpringitProperties springitProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
         System.out.println("Welcome to Springit!");
     }
 
+    @Bean
+    CommandLineRunner runner() {
+        return args -> System.out.println("Welcome message:" + springitProperties.getWelcomeMessage());
+    }
 }
