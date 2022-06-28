8469bc9b818ca1e81c887ae034aa9dbf6c6da9fe
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-06-29 01:06:45 +0430

    add logger to project

--- src/main/java/com/mohsen/springit/SpringitApplication.java
+++ src/main/java/com/mohsen/springit/SpringitApplication.java
@@ -1,14 +1,30 @@
 package com.mohsen.springit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringitApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
         System.out.println("Welcome to Springit!");
     }
 
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            log.error("error log");
+            log.warn("warn log");
+            log.info("info log");
+            log.debug("debug log");
+            log.trace("trace log");
+        };
+    }
 }
