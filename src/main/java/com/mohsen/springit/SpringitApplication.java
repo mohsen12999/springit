080033c362231e1fdc67748fec60cda3c7aa6269
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-06-28 22:32:03 +0430

    use profile in project

--- src/main/java/com/mohsen/springit/SpringitApplication.java
+++ src/main/java/com/mohsen/springit/SpringitApplication.java
@@ -1,7 +1,10 @@
 package com.mohsen.springit;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SpringitApplication {
@@ -11,4 +14,9 @@ public class SpringitApplication {
         System.out.println("Welcome to Springit!");
     }
 
+    @Bean
+    @Profile("dev")
+    CommandLineRunner runner() {
+        return args -> System.out.println("This is something that we would only do in dev.");
+    }
 }
