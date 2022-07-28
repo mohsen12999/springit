ae4669447f707a2a83d8d0048656df74cb676f1e
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-07-28 16:42:58 +0430

    Adding Mapping from Link to Comment

--- src/main/java/com/mohsen/springit/model/Comment.java
+++ src/main/java/com/mohsen/springit/model/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.util.Objects;
 
 @Entity
@@ -19,4 +20,6 @@ public class Comment {
     private String body;
 
     // link
+    @ManyToOne
+    private Link link;
 }
