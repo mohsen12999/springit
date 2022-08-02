7a11d57bb2cd56fc2110775132afafd6d6181d0d
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-08-03 02:08:03 +0430

    add a command line runner foe loading database info

---
+++ src/main/java/com/mohsen/springit/DatabaseLoader.java
@@ -0,0 +1,14 @@
+package com.mohsen.springit;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        // do some database work
+        System.out.println("Database loader...");
+    }
+}
