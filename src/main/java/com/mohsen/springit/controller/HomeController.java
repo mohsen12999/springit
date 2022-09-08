c4c2f4d1508e909dc84b90d066d223f0af643444
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-09-09 02:13:02 +0430

    add old template and add new template and files

--- src/main/java/com/mohsen/springit/controller/HomeController.java
+++ src/main/java/com/mohsen/springit/controller/HomeController.java
@@ -8,14 +8,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home() {
-        return "index";
-    }
-
-    @GetMapping("/hello")
-    public String hello(Model model) {
-        model.addAttribute("message","Hello World!");
-        return "index";
-    }
 }
